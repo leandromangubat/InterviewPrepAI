41cd1bc2a5c4921d3c7fb5905d304099deaacbbb
leandromangubat <leandromangubat@hotmail.com>
2026-05-04 20:33:25 -0400

    Updated Signup/Login Modal functions

--- frontend/interview-prep-ai/src/pages/Auth/Login.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
 
 const Login = ({ setCurrentPage }) => {
   const [email, setEmail] = useState("");
@@ -12,6 +13,27 @@ const Login = ({ setCurrentPage }) => {
   // Handle Login Form Submit
   const handleLogin = async (e) => {
     e.preventDefault();
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    //Login API Call
+    try {
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again");
+      }
+    }
   };
   return (
     <div className="w-[900vw] md:w-[33vw] p-7 flex flex-col justify-center">
--- frontend/interview-prep-ai/src/pages/LandingPage.jsx
+++ frontend/interview-prep-ai/src/pages/LandingPage.jsx
@@ -26,7 +26,7 @@ const LandingPage = () => {
               Interview Prep AI
             </div>
             <button
-              className="bg-linear-to-r from-[#FF9324] to-[#e99a4b] text-sm text-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer"
+              className="bg-linear-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer"
               onClick={() => setOpenAuthModal(true)}
             >
               Login / Sign Up
